98d234e8fa75a3c75112ccebc2e6aaaebb92dc1d
Mauznemo <mauznemo@gmail.com>
2024-07-25 18:14:57 +0200

    fix: displaying wrong extension

--- Assets/Scripts/UI/InfoDisplay.cs
+++ Assets/Scripts/UI/InfoDisplay.cs
@@ -10,32 +10,44 @@ public class InfoDisplay : MonoBehaviour, IPointerClickHandler
     [SerializeField] private TMP_Text firstText;
     [SerializeField] private TMP_Text secondText;
 
+    private float firstValue;
+    private float secondValue;
+    private string firstExtension = "km/h";
+    private string secondExtension = "K RPM";
+    private string firstFormat = "0";
+    private string secondFormat = "0.00";
 
     private bool isMain = true;
 
 
     public void SetFirstText(float value, string extension, string format = "0.00")
     {
+        firstValue = value;
+        firstExtension = extension;
+        firstFormat = format;
+
         if (isMain)
-            firstText.text = value.ToString(format) + " " + extension;
+            firstText.text = value.ToString(format);
         else
-            secondText.text = value.ToString(format);
+            secondText.text = value.ToString(format) + " " + extension;
     }
 
     public void SetSecondText(float value, string extension, string format = "0.00")
     {
-        if (isMain)
-            firstText.text = value.ToString(format) + " " + extension;
+        secondValue = value;
+        secondExtension = extension;
+        secondFormat = format;
+
+        if (!isMain)
+            firstText.text = value.ToString(format);
         else
-            secondText.text = value.ToString(format);
+            secondText.text = value.ToString(format) + " " + extension;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         isMain = !isMain;
-        string textOne = firstText.text;
-        string textTwo = secondText.text;
-        firstText.text = textTwo;
-        secondText.text = textOne;
+        SetFirstText(firstValue, firstExtension, firstFormat);
+        SetSecondText(secondValue, secondExtension, secondFormat);
     }
 }
