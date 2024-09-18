7b088c199152998d92ba95c3fe72e872f3163748
Mauznemo <mauznemo@gmail.com>
2024-09-18 19:08:12 +0200

    fix: control wheel/Rotary Encoder sending wrong dir sometimes

--- Assets/Scripts/ControlwheelManager.cs
+++ Assets/Scripts/ControlwheelManager.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using SmartifyOS.Audio;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ControlwheelManager : MonoBehaviour
 {
     private Mode mode = Mode.Audio;
+    private List<int> lastDirections = new List<int>();
 
     private void Start()
     {
@@ -21,7 +24,14 @@ public class ControlwheelManager : MonoBehaviour
 
     private void OnControlwheelChanged(int dir)
     {
-        switch (dir)
+        lastDirections.Add(dir);
+
+        if (lastDirections.Count > 4)
+        {
+            lastDirections.RemoveAt(0);
+        }
+
+        switch (GetCurrentDirection())
         {
             case 1:
                 OnIncreased();
@@ -32,12 +42,32 @@ public class ControlwheelManager : MonoBehaviour
         }
     }
 
+    //The Rotary Encoder sometimes glitches a bit and sends a wrong direction at the end or in between, so is to prevent that.
+    private int GetCurrentDirection()
+    {
+        int countNegative = lastDirections.Count(x => x == -1);
+        int countPositive = lastDirections.Count(x => x == 1);
+
+        if (countNegative > countPositive)
+        {
+            return -1;
+        }
+        else if (countPositive > countNegative)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
     private async void OnIncreased()
     {
         switch (mode)
         {
             case Mode.Audio:
-                await AudioManager.Instance.SetSystemVolumeWithOverlay(GetVolume() + 0.1f);
+                await AudioManager.Instance.SetSystemVolumeWithOverlay(GetVolume() + 5f);
                 break;
         }
     }
@@ -47,7 +77,7 @@ public class ControlwheelManager : MonoBehaviour
         switch (mode)
         {
             case Mode.Audio:
-                await AudioManager.Instance.SetSystemVolumeWithOverlay(GetVolume() - 0.1f);
+                await AudioManager.Instance.SetSystemVolumeWithOverlay(GetVolume() - 5f);
                 break;
         }
     }
