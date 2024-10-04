c3c7022ecabf00bbc01bbdc3b1b14b70856a8699
Mauznemo <mauznemo@gmail.com>
2024-10-04 21:00:52 +0200

    docs: for AudioManager

--- Assets/SmartifyOS/Scripts/System/Audio/AudioManager.cs
+++ Assets/SmartifyOS/Scripts/System/Audio/AudioManager.cs
@@ -16,6 +16,7 @@ namespace SmartifyOS.Audio
         /// <summary>Called when the volume is changed with <see cref="SetSystemVolumeWithOverlay"/> method</summary>
         public static event Action<float> OnVolumeChangedOverlay;
 
+        /// <summary> Returns the <see cref="AudioConfig_SO"/></summary>
         public static AudioConfig_SO audioConfig_SO => Instance._audioConfig_SO;
 
         [SerializeField] private AudioConfig_SO _audioConfig_SO;
@@ -34,11 +35,18 @@ namespace SmartifyOS.Audio
             warningAudioSource.clip = audioConfig_SO.notificationSounds.warningLoop;
         }
 
+        /// <summary>
+        /// Plays the <see cref="audioClip"/>
+        /// </summary>
+        /// <param name="audioClip">Audio clip to play</param>
         public void PlaySound(AudioClip audioClip)
         {
             soundAudioSource.PlayOneShot(audioClip);
         }
 
+        /// <summary>
+        /// Starts the <see cref="AudioConfig_SO.notificationSounds.warningLoop"/>
+        /// </summary>
         public void StartWarningSound()
         {
             playingWarningSound = true;
@@ -46,6 +54,9 @@ namespace SmartifyOS.Audio
             warningAudioSource.Play();
         }
 
+        /// <summary>
+        /// Stops the <see cref="AudioConfig_SO.notificationSounds.warningLoop"/>
+        /// </summary>
         public void StopWarningSound()
         {
             playingWarningSound = false;
@@ -53,12 +64,20 @@ namespace SmartifyOS.Audio
             warningAudioSource.Stop();
         }
 
+        /// <summary>
+        /// Sets the system output volume with an overlay (useful for external audio control)
+        /// </summary>
+        /// <param name="volume">Volume in percentage</param>
         public async Task SetSystemVolumeWithOverlay(float volume)
         {
             await SetSystemVolume(volume);
             OnVolumeChangedOverlay?.Invoke(volume);
         }
 
+        /// <summary>
+        /// Sets the system output volume
+        /// </summary>
+        /// <param name="volume">Volume in percentage</param>
         public async Task SetSystemVolume(float volume)
         {
             float maxClamp = SaveManager.Load().system.allowOverAmplification ? 100 : 150;
@@ -68,6 +87,10 @@ namespace SmartifyOS.Audio
             SaveManager.Load().system.audioVolume = volume;
         }
 
+        /// <summary>
+        /// Gets the system output volume
+        /// </summary>
+        /// <returns>System output volume in percentage</returns>
         public float GetSystemVolume()
         {
             return SaveManager.Load().system.audioVolume;
