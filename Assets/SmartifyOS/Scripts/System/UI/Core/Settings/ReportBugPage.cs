31c38b2c04ea76381dfde96957ad9679e88ec709
Mauznemo <mauznemo@gmail.com>
2024-12-07 14:22:14 +0100

    feat: add bug report qrcode to system settings

---
+++ Assets/SmartifyOS/Scripts/System/UI/Core/Settings/ReportBugPage.cs
@@ -0,0 +1,14 @@
+namespace SmartifyOS.Settings
+{
+    public class ReportBugPage : BaseSettingsPage
+    {
+        //Put your page logic here or leave empty if page only lists link buttons
+
+        //Gets called when the page opens
+        /*protected override void OnOpened()
+        {
+            
+        }*/
+    }
+
+}
