fbf4761ee3b7d84da33ff87139fe4f42d146c563
Mauznemo <mauznemo@gmail.com>
2024-12-07 18:49:14 +0100

    refactor: editor namespaces

--- Assets/SmartifyOS/Scripts/EditorTools/Editor/Windows/Export.cs
+++ Assets/SmartifyOS/Scripts/EditorTools/Editor/Windows/Export.cs
@@ -6,312 +6,310 @@ using UnityEngine;
 using System.IO;
 using SmartifyOS.Editor.Styles;
 using System;
-using SmartifyOS;
 using System.Linq;
-using System.Diagnostics;
-using System.Threading.Tasks;
 
-
-public class Export : EditorWindow
+namespace SmartifyOS.Editor
 {
-    [MenuItem("SmartifyOS/SmartifyOS Export")]
-    public static void ShowWindow()
+    public class Export : EditorWindow
     {
-        var window = GetWindow<Export>("SmartifyOS Export");
-        //window.SetSize(700, 400);
-        window.Show();
-        window.minSize = new Vector2(300, 300);
-
-    }
-
-    private List<DirectoryInfo> usbDrives = new List<DirectoryInfo>();
-    private List<string> usbDriveTexts = new List<string>();
-
-    private readonly string[] exportTargets = new string[] { "Linux x86", "Linux Arm (Experimental)" };
-
-    private int selectedDrive = 0;
-    private int selectedTarget = 0;
-    private bool exporting = false;
-
-    private void OnEnable()
-    {
-        GetUSBDrives();
-    }
-
-    private void OnGUI()
-    {
-        Title();
-
-        GUILayout.Space(30);
-
-        if (usbDrives.Count < selectedDrive)
+        [MenuItem("SmartifyOS/SmartifyOS Export")]
+        public static void ShowWindow()
         {
-            selectedDrive = 0;
+            var window = GetWindow<Export>("SmartifyOS Export");
+            //window.SetSize(700, 400);
+            window.Show();
+            window.minSize = new Vector2(300, 300);
+
         }
 
-        EditorGUILayout.LabelField("Select Target USB Drive");
-        if (usbDrives.Count > 0)
+        private List<DirectoryInfo> usbDrives = new List<DirectoryInfo>();
+        private List<string> usbDriveTexts = new List<string>();
+
+        private readonly string[] exportTargets = new string[] { "Linux x86", "Linux Arm (Experimental)" };
+
+        private int selectedDrive = 0;
+        private int selectedTarget = 0;
+        private bool exporting = false;
+
+        private void OnEnable()
         {
-            GUILayout.BeginHorizontal();
+            GetUSBDrives();
+        }
+
+        private void OnGUI()
+        {
+            Title();
+
+            GUILayout.Space(30);
+
+            if (usbDrives.Count < selectedDrive)
             {
+                selectedDrive = 0;
+            }
+
+            EditorGUILayout.LabelField("Select Target USB Drive");
+            if (usbDrives.Count > 0)
+            {
+                GUILayout.BeginHorizontal();
+                {
 
 
-                selectedDrive = EditorGUILayout.Popup(selectedDrive, usbDriveTexts.ToArray(), Style.Popup, GUILayout.Height(30));
-                if (GUILayout.Button("Refresh", GUILayout.MaxWidth(60), GUILayout.Height(30)))
+                    selectedDrive = EditorGUILayout.Popup(selectedDrive, usbDriveTexts.ToArray(), Style.Popup, GUILayout.Height(30));
+                    if (GUILayout.Button("Refresh", GUILayout.MaxWidth(60), GUILayout.Height(30)))
+                    {
+                        GetUSBDrives();
+                    }
+                }
+                GUILayout.EndHorizontal();
+            }
+            else
+            {
+                EditorGUILayout.LabelField("No USB Drives Found");
+                if (GUILayout.Button("Search for USB Drives"))
                 {
                     GetUSBDrives();
                 }
             }
-            GUILayout.EndHorizontal();
-        }
-        else
-        {
-            EditorGUILayout.LabelField("No USB Drives Found");
-            if (GUILayout.Button("Search for USB Drives"))
+
+
+            GUILayout.Space(30);
+
+            EditorGUILayout.LabelField("Export Target");
+            selectedTarget = EditorGUILayout.Popup(selectedTarget, exportTargets, Style.Popup, GUILayout.Height(30));
+
+            GUILayout.Space(20);
+
+            if (selectedTarget == 1)
             {
-                GetUSBDrives();
+                EditorGUILayout.LabelField("Linux Arm is not yet supported");
+            }
+
+            GUI.enabled = usbDrives.Count > 0 && selectedTarget != 1 && !exporting;
+
+
+            if (GUILayout.Button("Export Installer", Style.Button, GUILayout.Height(30)))
+            {
+                ExportInstaller();
+            }
+
+            if (GUILayout.Button("Export Update", Style.Button, GUILayout.Height(30)))
+            {
+                ExportUpdate();
+            }
+
+            GUI.enabled = true;
+
+            if (exporting)
+            {
+                EditorGUILayout.LabelField("Exporting...");
             }
         }
 
-
-        GUILayout.Space(30);
-
-        EditorGUILayout.LabelField("Export Target");
-        selectedTarget = EditorGUILayout.Popup(selectedTarget, exportTargets, Style.Popup, GUILayout.Height(30));
-
-        GUILayout.Space(20);
-
-        if (selectedTarget == 1)
+        private async void ExportInstaller()
         {
-            EditorGUILayout.LabelField("Linux Arm is not yet supported");
-        }
+            exporting = true;
+            string usbPath = usbDrives[selectedDrive].FullName;
 
-        GUI.enabled = usbDrives.Count > 0 && selectedTarget != 1 && !exporting;
+            if (!Directory.Exists(usbPath))
+                return;
 
+            bool cloneSuccess = await Git.CloneRepository("https://github.com/Mauznemo/SmartifyOS-Installer.git", $"{usbPath}/SmartifyOS-Installer/");
 
-        if (GUILayout.Button("Export Installer", Style.Button, GUILayout.Height(30)))
-        {
-            ExportInstaller();
-        }
-
-        if (GUILayout.Button("Export Update", Style.Button, GUILayout.Height(30)))
-        {
-            ExportUpdate();
-        }
-
-        GUI.enabled = true;
-
-        if (exporting)
-        {
-            EditorGUILayout.LabelField("Exporting...");
-        }
-    }
-
-    private async void ExportInstaller()
-    {
-        exporting = true;
-        string usbPath = usbDrives[selectedDrive].FullName;
-
-        if (!Directory.Exists(usbPath))
-            return;
-
-        bool cloneSuccess = await Git.CloneRepository("https://github.com/Mauznemo/SmartifyOS-Installer.git", $"{usbPath}/SmartifyOS-Installer/");
-
-        if (!cloneSuccess)
-        {
-            EditorUtility.DisplayDialog("Error", "Failed to clone repository", "Okay");
-            exporting = false;
-            return;
-        }
-
-        UnityEngine.Debug.Log("Cloned repository");
-
-        if (!Directory.Exists($"{usbPath}/SmartifyOS-Installer/"))
-        {
-            UnityEngine.Debug.LogError("Couldn't find SmartifyOS-Installer folder");
-            return;
-        }
-
-        if (!Directory.Exists($"{usbPath}/SmartifyOS-Installer/SmartifyOS/GUI/"))
-        {
-            Directory.CreateDirectory($"{usbPath}/SmartifyOS-Installer/SmartifyOS/GUI/");
-        }
-
-        BuildTo($"{usbPath}/SmartifyOS-Installer/SmartifyOS/GUI/");
-
-        exporting = false;
-    }
-
-    private void ExportUpdate()
-    {
-        exporting = true;
-        string usbPath = usbDrives[selectedDrive].FullName;
-
-        if (!Directory.Exists(usbPath))
-            return;
-
-        string buildPath = usbPath + "/smartify_os_update/";
-
-        if (!Directory.Exists(buildPath))
-        {
-            Directory.CreateDirectory(buildPath);
-        }
-
-        BuildTo(buildPath);
-    }
-
-    private void BuildTo(string path)
-    {
-        if (!Directory.Exists(path))
-            return;
-        DeleteSubFolders(path);
-
-        PlayerSettings.bundleVersion = GetBuildVersion();
-
-        BuildOptions buildOptions = BuildOptions.None;
-        string[] scenePaths = EditorBuildSettings.scenes
-                .Where(scene => scene.enabled)
-                .Select(scene => scene.path)
-                .ToArray();
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
-        {
-            scenes = scenePaths,
-            locationPathName = $"{path}SmartifyOS.x86_64",
-            target = BuildTarget.StandaloneLinux64,
-            options = buildOptions,
-            //subtarget = (int)buildSubtarget
-
-        };
-
-        var buildReport = BuildPipeline.BuildPlayer(buildPlayerOptions);
-
-        if (buildReport.summary.totalErrors == 0)
-        {
-            DeleteFoldersWithDoNotShip(path);
-
-            EditorUtility.DisplayDialog("Build Successful", "Build Successful", "Okay");
-        }
-        else
-        {
-            EditorUtility.DisplayDialog($"{buildReport.summary.totalErrors} Errors", $"{buildReport.summary.totalErrors} Errors during Build", "Okay");
-        }
-
-        exporting = false;
-    }
-
-    private void DeleteFoldersWithDoNotShip(string path)
-    {
-        DirectoryInfo directory = new DirectoryInfo(path);
-
-        if (!directory.Exists)
-        {
-            return;
-        }
-
-        foreach (DirectoryInfo subDirectory in directory.GetDirectories())
-        {
-            if (subDirectory.Name.ToLower().Contains("donotship"))
+            if (!cloneSuccess)
             {
-                subDirectory.Delete(true);
+                EditorUtility.DisplayDialog("Error", "Failed to clone repository", "Okay");
+                exporting = false;
+                return;
+            }
+
+            UnityEngine.Debug.Log("Cloned repository");
+
+            if (!Directory.Exists($"{usbPath}/SmartifyOS-Installer/"))
+            {
+                UnityEngine.Debug.LogError("Couldn't find SmartifyOS-Installer folder");
+                return;
+            }
+
+            if (!Directory.Exists($"{usbPath}/SmartifyOS-Installer/SmartifyOS/GUI/"))
+            {
+                Directory.CreateDirectory($"{usbPath}/SmartifyOS-Installer/SmartifyOS/GUI/");
+            }
+
+            BuildTo($"{usbPath}/SmartifyOS-Installer/SmartifyOS/GUI/");
+
+            exporting = false;
+        }
+
+        private void ExportUpdate()
+        {
+            exporting = true;
+            string usbPath = usbDrives[selectedDrive].FullName;
+
+            if (!Directory.Exists(usbPath))
+                return;
+
+            string buildPath = usbPath + "/smartify_os_update/";
+
+            if (!Directory.Exists(buildPath))
+            {
+                Directory.CreateDirectory(buildPath);
+            }
+
+            BuildTo(buildPath);
+        }
+
+        private void BuildTo(string path)
+        {
+            if (!Directory.Exists(path))
+                return;
+            DeleteSubFolders(path);
+
+            PlayerSettings.bundleVersion = GetBuildVersion();
+
+            BuildOptions buildOptions = BuildOptions.None;
+            string[] scenePaths = EditorBuildSettings.scenes
+                    .Where(scene => scene.enabled)
+                    .Select(scene => scene.path)
+                    .ToArray();
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+            {
+                scenes = scenePaths,
+                locationPathName = $"{path}SmartifyOS.x86_64",
+                target = BuildTarget.StandaloneLinux64,
+                options = buildOptions,
+                //subtarget = (int)buildSubtarget
+
+            };
+
+            var buildReport = BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+            if (buildReport.summary.totalErrors == 0)
+            {
+                DeleteFoldersWithDoNotShip(path);
+
+                EditorUtility.DisplayDialog("Build Successful", "Build Successful", "Okay");
             }
             else
             {
-                DeleteFoldersWithDoNotShip(subDirectory.FullName);
+                EditorUtility.DisplayDialog($"{buildReport.summary.totalErrors} Errors", $"{buildReport.summary.totalErrors} Errors during Build", "Okay");
+            }
+
+            exporting = false;
+        }
+
+        private void DeleteFoldersWithDoNotShip(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            if (!directory.Exists)
+            {
+                return;
+            }
+
+            foreach (DirectoryInfo subDirectory in directory.GetDirectories())
+            {
+                if (subDirectory.Name.ToLower().Contains("donotship"))
+                {
+                    subDirectory.Delete(true);
+                }
+                else
+                {
+                    DeleteFoldersWithDoNotShip(subDirectory.FullName);
+                }
             }
         }
-    }
 
-    private void DeleteSubFolders(string path)
-    {
-        DirectoryInfo directory = new DirectoryInfo(path);
-
-        if (!directory.Exists)
+        private void DeleteSubFolders(string path)
         {
-            return;
-        }
+            DirectoryInfo directory = new DirectoryInfo(path);
 
-        foreach (DirectoryInfo subDirectory in directory.GetDirectories())
-        {
-            DeleteSubFolders(subDirectory.FullName);
+            if (!directory.Exists)
+            {
+                return;
+            }
 
-            foreach (FileInfo file in subDirectory.GetFiles())
+            foreach (DirectoryInfo subDirectory in directory.GetDirectories())
+            {
+                DeleteSubFolders(subDirectory.FullName);
+
+                foreach (FileInfo file in subDirectory.GetFiles())
+                {
+                    file.Delete();
+                }
+
+                subDirectory.Delete(true);
+            }
+
+            foreach (FileInfo file in directory.GetFiles())
             {
                 file.Delete();
             }
-
-            subDirectory.Delete(true);
         }
 
-        foreach (FileInfo file in directory.GetFiles())
+        private void Title()
         {
-            file.Delete();
+            GUILayout.Space(30);
+            EditorGUILayout.LabelField("Export Application", Style.LargeHeading);
+
+            var descriptionStyle = new GUIStyle(EditorStyles.label);
+            descriptionStyle.padding = new RectOffset(5, 0, 0, 0);
+            descriptionStyle.wordWrap = true;
+
+            EditorGUILayout.LabelField("Export your application as an installer or update.", descriptionStyle);
         }
-    }
 
-    private void Title()
-    {
-        GUILayout.Space(30);
-        EditorGUILayout.LabelField("Export Application", Style.LargeHeading);
-
-        var descriptionStyle = new GUIStyle(EditorStyles.label);
-        descriptionStyle.padding = new RectOffset(5, 0, 0, 0);
-        descriptionStyle.wordWrap = true;
-
-        EditorGUILayout.LabelField("Export your application as an installer or update.", descriptionStyle);
-    }
-
-    private void GetDrivesLinux()
-    {
-        usbDrives.Clear();
-        usbDriveTexts.Clear();
-
-        var drivesString = LinuxCommand.Run("df -h | grep -E '^/dev/' | awk '{print $NF}'");
-        string[] drives = drivesString.Split('\n');
-
-
-        foreach (var drive in drives)
+        private void GetDrivesLinux()
         {
-            if (drive == "/boot/efi" || drive == "/" || string.IsNullOrEmpty(drive))
+            usbDrives.Clear();
+            usbDriveTexts.Clear();
+
+            var drivesString = LinuxCommand.Run("df -h | grep -E '^/dev/' | awk '{print $NF}'");
+            string[] drives = drivesString.Split('\n');
+
+
+            foreach (var drive in drives)
             {
-                continue;
+                if (drive == "/boot/efi" || drive == "/" || string.IsNullOrEmpty(drive))
+                {
+                    continue;
+                }
+                var driveInfo = new DirectoryInfo(drive);
+                usbDrives.Add(driveInfo);
+                usbDriveTexts.Add(driveInfo.Name);
             }
-            var driveInfo = new DirectoryInfo(drive);
-            usbDrives.Add(driveInfo);
-            usbDriveTexts.Add(driveInfo.Name);
 
         }
 
-    }
-
-    private void GetUSBDrives()
-    {
-        if (Environment.OSVersion.Platform == PlatformID.Unix)
+        private void GetUSBDrives()
         {
-            GetDrivesLinux();
-
-            return;
-        }
-
-        usbDrives.Clear();
-        usbDriveTexts.Clear();
-
-        DriveInfo[] allDrives = DriveInfo.GetDrives();
-
-
-        foreach (DriveInfo drive in allDrives)
-        {
-            if (drive.IsReady && drive.DriveType == DriveType.Removable)
+            if (Environment.OSVersion.Platform == PlatformID.Unix)
             {
-                usbDrives.Add(new DirectoryInfo(drive.Name));
-                usbDriveTexts.Add($"{drive.Name} ({drive.TotalSize.SizeConvert()}, {(drive.TotalFreeSpace * 100) / drive.TotalSize}% free)");
+                GetDrivesLinux();
+
+                return;
+            }
+
+            usbDrives.Clear();
+            usbDriveTexts.Clear();
+
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+
+            foreach (DriveInfo drive in allDrives)
+            {
+                if (drive.IsReady && drive.DriveType == DriveType.Removable)
+                {
+                    usbDrives.Add(new DirectoryInfo(drive.Name));
+                    usbDriveTexts.Add($"{drive.Name} ({drive.TotalSize.SizeConvert()}, {(drive.TotalFreeSpace * 100) / drive.TotalSize}% free)");
+                }
             }
         }
-    }
 
-    private string GetBuildVersion()
-    {
-        DateTime now = DateTime.Now;
-        string version = $"{now.Year}.{now.Month:D2}.{now.Day:D2}.{now.Hour:D2}{now.Minute:D2}";
-        return version;
+        private string GetBuildVersion()
+        {
+            DateTime now = DateTime.Now;
+            string version = $"{now.Year}.{now.Month:D2}.{now.Day:D2}.{now.Hour:D2}{now.Minute:D2}";
+            return version;
+        }
     }
 }
--- Assets/SmartifyOS/Scripts/EditorTools/Editor/Windows/Updater.cs
+++ Assets/SmartifyOS/Scripts/EditorTools/Editor/Windows/Updater.cs
@@ -1,180 +1,182 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing.Drawing2D;
 using System.Threading.Tasks;
-using SmartifyOS.Editor;
 using SmartifyOS.Editor.Styles;
 using UnityEditor;
 using UnityEngine;
 
-public class Updater : EditorWindow
+namespace SmartifyOS.Editor
 {
-    private Type type;
-    private bool isChecking = false;
-    private bool isUpdating = false;
-
-    List<Git.Diff> diffs = new List<Git.Diff>();
-
-    private Vector2 scrollPosition;
-
-    // Add a menu item to show the Git Update Checker window
-    [MenuItem("SmartifyOS/Check for Updates")]
-    public static void ShowWindow()
+    public class Updater : EditorWindow
     {
-        var window = GetWindow<Updater>("Updater (Experimental)");
-        window.minSize = new Vector2(500, 300);
-    }
+        private Type type;
+        private bool isChecking = false;
+        private bool isUpdating = false;
 
-    private async void OnEnable()
-    {
-        diffs.Clear();
-        await CheckType();
-    }
+        List<Git.Diff> diffs = new List<Git.Diff>();
 
-    private void OnGUI()
-    {
-        GUILayout.Label($"Check for update (Repository type: {type})", EditorStyles.boldLabel);
-        GUI.color = Color.gray;
-        GUILayout.Label($"This updater is still in an experimental stage. Please be careful and report any bugs", EditorStyles.wordWrappedLabel);
-        GUI.color = Color.white;
+        private Vector2 scrollPosition;
 
-        GUILayout.Space(20);
-
-        if (diffs.Count > 0)
+        // Add a menu item to show the Git Update Checker window
+        [MenuItem("SmartifyOS/Check for Updates")]
+        public static void ShowWindow()
         {
-            GUILayout.Label($"These files are different or new (files will only be deleted if you haven't modified them):", EditorStyles.wordWrappedLabel);
-            ShowDiff();
+            var window = GetWindow<Updater>("Updater (Experimental)");
+            window.minSize = new Vector2(500, 300);
         }
 
-        GUILayout.FlexibleSpace();
-
-        if (diffs.Count < 1)
+        private async void OnEnable()
         {
-            if (!isChecking && GUILayout.Button("Check for Update", Style.Button, GUILayout.Height(30)))
+            diffs.Clear();
+            await CheckType();
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label($"Check for update (Repository type: {type})", EditorStyles.boldLabel);
+            GUI.color = Color.gray;
+            GUILayout.Label($"This updater is still in an experimental stage. Please be careful and report any bugs", EditorStyles.wordWrappedLabel);
+            GUI.color = Color.white;
+
+            GUILayout.Space(20);
+
+            if (diffs.Count > 0)
             {
-                CheckForUpdates();
+                GUILayout.Label($"These files are different or new (files will only be deleted if you haven't modified them):", EditorStyles.wordWrappedLabel);
+                ShowDiff();
+            }
+
+            GUILayout.FlexibleSpace();
+
+            if (diffs.Count < 1)
+            {
+                if (!isChecking && GUILayout.Button("Check for Update", Style.Button, GUILayout.Height(30)))
+                {
+                    CheckForUpdates();
+                }
+            }
+            else
+            {
+                if (!isUpdating && GUILayout.Button("Sync Repository", Style.Button, GUILayout.Height(30)))
+                {
+                    SyncRepository();
+                }
             }
         }
-        else
+
+        // Check for updates from the remote repository
+        private async void CheckForUpdates()
         {
-            if (!isUpdating && GUILayout.Button("Sync Repository", Style.Button, GUILayout.Height(30)))
+            isChecking = true;
+
+            await CheckType();
+
+            // Fetch the latest changes from the remote repository
+            await Git.Command($"fetch {GetRemote()}");
+
+            int ahead = 0;
+            int behind = 0;
+
+            if (type == Type.Fork)
             {
-                SyncRepository();
+                ahead = await Git.GetAhead("upstream/main");
+                behind = await Git.GetBehind("upstream/main");
+                if (behind > 0)
+                {
+                    bool update = EditorUtility.DisplayDialog("Update", $"You are {behind} commits behind {GetRemote()}. Would you like to update?", "Yes", "No");
+                }
             }
-        }
-    }
+            else if (type == Type.Clone)
+            {
+                ahead = await Git.GetAhead("origin/main");
+                behind = await Git.GetBehind("origin/main");
+            }
 
-    // Check for updates from the remote repository
-    private async void CheckForUpdates()
-    {
-        isChecking = true;
-
-        await CheckType();
-
-        // Fetch the latest changes from the remote repository
-        await Git.Command($"fetch {GetRemote()}");
-
-        int ahead = 0;
-        int behind = 0;
-
-        if (type == Type.Fork)
-        {
-            ahead = await Git.GetAhead("upstream/main");
-            behind = await Git.GetBehind("upstream/main");
             if (behind > 0)
             {
                 bool update = EditorUtility.DisplayDialog("Update", $"You are {behind} commits behind {GetRemote()}. Would you like to update?", "Yes", "No");
+
+                if (update)
+                {
+                    diffs = await Git.GetDiffs();
+                }
             }
-        }
-        else if (type == Type.Clone)
-        {
-            ahead = await Git.GetAhead("origin/main");
-            behind = await Git.GetBehind("origin/main");
-        }
-
-        if (behind > 0)
-        {
-            bool update = EditorUtility.DisplayDialog("Update", $"You are {behind} commits behind {GetRemote()}. Would you like to update?", "Yes", "No");
-
-            if (update)
+            else
             {
-                diffs = await Git.GetDiffs();
+                EditorUtility.DisplayDialog("Update", $"Your repository is up to date \n\n({ahead} commits ahead and {behind} commits behind {GetRemote()}).", "Ok");
             }
+            isChecking = false;
+            Repaint();
         }
-        else
-        {
-            EditorUtility.DisplayDialog("Update", $"Your repository is up to date \n\n({ahead} commits ahead and {behind} commits behind {GetRemote()}).", "Ok");
-        }
-        isChecking = false;
-        Repaint();
-    }
 
-    private void ShowDiff()
-    {
-        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
-
-        foreach (Git.Diff diff in diffs)
+        private void ShowDiff()
         {
-            string diffType = "";
-            switch (diff.diffType)
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+
+            foreach (Git.Diff diff in diffs)
             {
-                case Git.DiffType.Added:
-                    GUI.color = Color.green;
-                    diffType = "A";
-                    break;
-                case Git.DiffType.Modified:
-                    GUI.color = Color.yellow;
-                    diffType = "M";
-                    break;
-                case Git.DiffType.Deleted:
-                    GUI.color = Color.red;
-                    diffType = "D";
-                    break;
+                string diffType = "";
+                switch (diff.diffType)
+                {
+                    case Git.DiffType.Added:
+                        GUI.color = Color.green;
+                        diffType = "A";
+                        break;
+                    case Git.DiffType.Modified:
+                        GUI.color = Color.yellow;
+                        diffType = "M";
+                        break;
+                    case Git.DiffType.Deleted:
+                        GUI.color = Color.red;
+                        diffType = "D";
+                        break;
+                }
+
+                GUILayout.Label($"({diffType}) {diff.name}");
+
+                GUI.color = Color.white;
             }
 
-            GUILayout.Label($"({diffType}) {diff.name}");
-
-            GUI.color = Color.white;
+            EditorGUILayout.EndScrollView();
         }
 
-        EditorGUILayout.EndScrollView();
-    }
-
-    private async Task CheckType()
-    {
-        if (await Git.HasUpstream())
+        private async Task CheckType()
         {
-            type = Type.Fork;
+            if (await Git.HasUpstream())
+            {
+                type = Type.Fork;
+            }
+            else
+            {
+                type = Type.Clone;
+            }
         }
-        else
+
+        private string GetRemote()
         {
-            type = Type.Clone;
+            return type == Type.Fork ? "upstream" : "origin";
         }
-    }
 
-    private string GetRemote()
-    {
-        return type == Type.Fork ? "upstream" : "origin";
-    }
+        // Sync the local repository with remote changes
+        private async void SyncRepository()
+        {
+            isUpdating = true;
 
-    // Sync the local repository with remote changes
-    private async void SyncRepository()
-    {
-        isUpdating = true;
+            // Pull the latest changes
+            string output = await Git.Command($"pull {GetRemote()} main");
 
-        // Pull the latest changes
-        string output = await Git.Command($"pull {GetRemote()} main");
+            EditorUtility.DisplayDialog("Update", output, "Ok");
 
-        EditorUtility.DisplayDialog("Update", output, "Ok");
+            diffs.Clear();
 
-        diffs.Clear();
+            isUpdating = false;
+        }
 
-        isUpdating = false;
-    }
-
-    private enum Type
-    {
-        Clone,
-        Fork
+        private enum Type
+        {
+            Clone,
+            Fork
+        }
     }
 }
+
